3b81966b36e9c22b3d7d098bf8ace9384ad8e427
Fergus Baker <fergusbkr@gmail.com>
2022-07-26 22:41:43 +0100

    fixed julia model example

--- src/julia-models/additive.jl
+++ src/julia-models/additive.jl
@@ -4,8 +4,20 @@
     a::Float64 = 0.5
 end
 
-function invokemodel(m::PowerLaw, energy::AbstractArray)
-    energy .^ (-m.a)
+# this is what i would like the implementation to be
+# function invokemodel!(output, err, energy, m::PowerLaw)
+#     @. output = energy ^ (-m.a)
+# end
+
+# this is what it has to be right now
+function invokemodel!(output, err, energy, m::PowerLaw)
+    alpha = 1 - m.a
+    alpha_inv = inv(alpha)
+    @. output = alpha_inv * energy^alpha
+    # this is the normalisation needed to ensure flux is
+    # integrable over energy bins
+    @views output[1:end-1] .= output[2:end] .- output[1:end-1]
+    output
 end
 
 export PowerLaw
