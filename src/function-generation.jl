8e928d3eae8b18b5f5a1f60b825f2c3d46073df0
Fergus Baker <fergusbkr@gmail.com>
2022-08-06 22:15:28 +0100

    aggregate struct for function generation implementation

--- src/function-generation.jl
+++ src/function-generation.jl
@@ -1,46 +1,79 @@
+# assemble_invoke_statment!(params, flux, M)
+# parameter_symbols_and_types(M)
+
+mutable struct GenerationAggragate
+    statements::Vector{Expr}
+    parameters::Vector{Symbol}
+    closure_parameters::Vector{Symbol}
+    models::Vector{Type}
+    flux_count::Int
+    maximum_flux_count::Int
+    GenerationAggragate() = new(Expr[], Symbol[], Symbol[], Type[], 0, 0)
+end
+
+push_closure_param!(g::GenerationAggragate, s::Symbol) = push!(g.closure_parameters, s)
+push_param!(g::GenerationAggragate, s::Symbol) = push!(g.parameters, s)
+push_statement!(g::GenerationAggragate, s::Expr) = push!(g.statements, s)
+push_model!(g::GenerationAggragate, t::Type) = push!(g.models, t)
+
+function new_parameter!(ga::GenerationAggragate, p::Symbol)
+    param = Base.gensym(p)
+    push_param!(ga, param)
+    param
+end
+
+get_flux_symbol(i::Int) = Symbol(:flux, i)
+function get_flux_symbol!(g::GenerationAggragate)
+    i = inc_flux!(g)
+    Symbol(:flux, i)
+end
+
+function set_flux!(g::GenerationAggragate, f)
+    g.flux_count = f
+    if g.flux_count > g.maximum_flux_count
+        g.maximum_flux_count = g.flux_count
+    end
+    g.flux_count
+end
+
+inc_flux!(g::GenerationAggragate) = set_flux!(g, g.flux_count + 1)
+dec_flux!(g::GenerationAggragate) = set_flux!(g, g.flux_count - 1)
+
+
 # model parameter introspection
 
-parameter_symbols_and_types(M::Type{<:AbstractSpectralModel}) = (fieldnames(M), M.types)
+model_parameter_types(M::Type{<:AbstractSpectralModel}) = M.types
 
-push_symbol_and_type!(_, _, ::Type{Nothing}) = nothing
-function push_symbol_and_type!(names, types, ::Type{M}) where {M<:AbstractSpectralModel}
-    for (s, t) in zip(parameter_symbols_and_types(M)...)
-        push!(names, s)
+push_parameter_types!(_, ::Type{Nothing}) = nothing
+function push_parameter_types!(types, M::Type{<:AbstractSpectralModel})
+    for t in model_parameter_types(M)
         push!(types, t)
     end
 end
 
-function model_parameter_symbol_and_type(
+function model_parameter_types(
     model::Type{<:SpectralFitting.CompositeSpectralModel},
 )
-    names = Symbol[]
     types = Type[]
     SpectralFitting.recursive_model_parse(model) do (left, right, _)
-        push_symbol_and_type!(names, types, right)
-        push_symbol_and_type!(names, types, left)
+        push_parameter_types!(types, right)
+        push_parameter_types!(types, left)
     end
-    names, types
-end
-
-function model_parameter_symbol_and_type(model::Type{<:AbstractSpectralModel})
-    names = Symbol[]
-    types = Type[]
-    push_symbol_and_type!(names, types, model)
-    names, types
+    types
 end
 
 function model_parameter_count(M::Type{<:AbstractSpectralModel})::Int
-    _, types = model_parameter_symbol_and_type(M)
+    types = model_parameter_types(M)
     length(types)
 end
 
 function model_frozen_parameter_count(M::Type{<:AbstractSpectralModel})::Int
-    _, types = model_parameter_symbol_and_type(M)
+    types = model_parameter_types(M)
     count(is_frozen, types)
 end
 
 function model_free_parameter_count(M::Type{<:AbstractSpectralModel})::Int
-    _, types = model_parameter_symbol_and_type(M)
+    types = model_parameter_types(M)
     count(!is_frozen, types)
 end
 
@@ -62,83 +95,59 @@ end
 # model function assembly
 
 function assemble_invoke_statment!(
-    parameters,
+    ga::GenerationAggragate,
     flux,
-    ::Type{M},
-) where {M<:AbstractSpectralModel}
-    params = Symbol[]
-
-    for p in fieldnames(M)
-        i = Base.gensym(p)
-        push!(parameters, i)
-        push!(params, i)
+    M::Type{<:AbstractSpectralModel},
+)
+    params = map(fieldnames(M)) do p
+        new_parameter!(ga, p)
     end
 
     base_name = Base.typename(M).name
-    :(invokemodel!($flux, energy, $(base_name){Float64}, $(params...)))
+    :(invokemodel!($flux, energy, $(base_name), $(params...)))
 end
 
-function add_flux_invokation!(flux_count, parameters, M, K::Convolutional)
-    flux = Symbol(:flux, flux_count[])
-    assemble_invoke_statment!(parameters, flux, M)
+function add_flux_invokation!(ga::GenerationAggragate, M, ::Convolutional)
+    flux = get_flux_symbol(ga.flux_count)
+    s = assemble_invoke_statment!(ga, flux, M)
+    push_statement!(ga, s)
 end
 
-function add_flux_invokation!(flux_count, parameters, M, K)
-    i = (flux_count[] += 1)
-    flux = Symbol(:flux, i)
-    assemble_invoke_statment!(parameters, flux, M)
+function add_flux_invokation!(ga::GenerationAggragate, M, ::AbstractSpectralModelKind)
+    inc_flux!(ga)
+    flux = get_flux_symbol(ga.flux_count)
+    s = assemble_invoke_statment!(ga, flux, M)
+    push_statement!(ga, s)
 end
 
-function add_flux_invokation!(
-    flux_count,
-    parameters,
-    ::Type{M},
-) where {M<:AbstractSpectralModel}
-    add_flux_invokation!(flux_count, parameters, M, modelkind(M))
+add_flux_invokation!(ga::GenerationAggragate, M::Type{<:AbstractSpectralModel}) =
+    add_flux_invokation!(ga, M, modelkind(M))
+
+assemble_and_add_resolve_flux_combine!(::GenerationAggragate, ::Type{ConvolutionOperator}) = nothing
+assemble_and_add_resolve_flux_combine!( ga::GenerationAggragate, O::Type{<:AbstractCompositeOperator}) =
+    assemble_and_add_resolve_flux_combine!(ga, operation_symbol(O))
+
+function assemble_and_add_resolve_flux_combine!(ga::GenerationAggragate, op::Symbol)
+    dec_flux!(ga)
+    fr = get_flux_symbol(ga.flux_count + 1)
+    fl = get_flux_symbol(ga.flux_count)
+    expr = Expr(:call, op, fl, fr)
+    push_statement!(ga, :(@.($fl = $expr)))
 end
 
-push_resolve_flux_combine!(_, _, ::Type{ConvolutionOperator}) = nothing
-function push_resolve_flux_combine!(
-    statements,
-    flux_count,
-    ::Type{O},
-) where {O<:AbstractCompositeOperator}
-    push_resolve_flux_combine!(statements, flux_count, operation_symbol(O))
-end
-
-function push_resolve_flux_combine!(statements, flux_count, op::Symbol)
-    i = (flux_count[] -= 1)
-    flux_right = Symbol(:flux, i + 1)
-    flux_left = Symbol(:flux, i)
-    expr = Expr(:call, op, flux_left, flux_right)
-    push!(statements, :(@.($flux_left = $expr)))
-end
-
-push_model!(_, _, _, ::Type{Nothing}) = nothing
-function push_model!(
-    statements,
-    parameters,
-    flux_count,
-    model::Type{<:AbstractSpectralModel},
-)
-    push!(statements, add_flux_invokation!(flux_count, parameters, model))
-end
+assemble_and_add_model!(::GenerationAggragate, ::Type{Nothing}) = nothing
+assemble_and_add_model!(ga::GenerationAggragate, model::Type{<:AbstractSpectralModel}) =
+    add_flux_invokation!(ga, model)
 
 __adjust_max!(flux_max, flux_counts) = flux_max[] = (flux_counts[] > flux_max[] ? flux_counts[] : flux_max[])
 function assemble_expression(model::Type{<:CompositeSpectralModel})
-    flux_count = Ref(0)
-    flux_count_max = Ref(0)
-    statements = Expr[]
-    parameters = Symbol[]
+    ga = GenerationAggragate()
     recursive_model_parse(model) do (left, right, op)
-        push_model!(statements, parameters, flux_count, right)
-        __adjust_max!(flux_count_max, flux_count)
-        push_model!(statements, parameters, flux_count, left)
-        __adjust_max!(flux_count_max, flux_count)
-        push_resolve_flux_combine!(statements, flux_count, op)
-        __adjust_max!(flux_count_max, flux_count)
+        assemble_and_add_model!(ga, right)
+        assemble_and_add_model!(ga, left)
+        assemble_and_add_resolve_flux_combine!(ga, op)
     end
-    statements, parameters, flux_count_max[]
+    return ga
 end
 
 """
@@ -147,14 +156,16 @@ end
 Specialisation for when only a single model invoked.
 """
 function assemble_expression(model::Type{<:AbstractSpectralModel})
-    parameters = Symbol[]
-    s = assemble_invoke_statment!(parameters, :flux1, model)
-    (s,), parameters
+    ga = GenerationAggragate()
+    flux = get_flux_symbol(inc_flux!(ga))
+    s = assemble_invoke_statment!(ga, flux, model)
+    push_statement!(ga, s)
+    ga
 end
 
 function maximum_flux_count(model)
-    _, _, fc = assemble_expression(model)
-    fc
+    ga = assemble_expression(model)
+    ga.maximum_flux_count
 end
 
 @generated function generated_maximum_flux_count(model)
@@ -163,14 +174,14 @@ end
 end
 
 @generated function generated_model_call!(fluxes, energy, model, params)
-    expr, parameters, _ = assemble_expression(model)
-    flux_unpack = [Symbol(:flux, i) for i in 1:maximum_flux_count(model)]
-    p_assign = [:($s = params[$i]) for (i, s) in enumerate(parameters)]
+    ga = assemble_expression(model)
+    flux_unpack = [Symbol(:flux, i) for i in 1:ga.maximum_flux_count]
+    p_assign = [:($s = params[$i]) for (i, s) in enumerate(ga.parameters)]
     quote
         @fastmath begin
             @inbounds let ($(flux_unpack...),) = fluxes
                 $(p_assign...)
-                $(expr...)
+                $(ga.statements...)
                 return flux1
             end
         end
@@ -178,15 +189,15 @@ end
 end
 
 @generated function generated_model_call!(fluxes, energy, model, free_params, frozen_params)
-    expr, parameters, _ = assemble_expression(model)
-    _, p_types = model_parameter_symbol_and_type(model)
+    ga = assemble_expression(model)
+    p_types = model_parameter_types(model)
 
-    flux_unpack = [Symbol(:flux, i) for i in 1:maximum_flux_count(model)]
+    flux_unpack = [Symbol(:flux, i) for i in 1:ga.maximum_flux_count]
 
     # unpack free and frozen seperately
     i_frozen = 0
     i_free = 0
-    p_assign = map(enumerate(parameters)) do (i, s)
+    p_assign = map(enumerate(ga.parameters)) do (i, s)
         if is_frozen(p_types[i])
             :($s = frozen_params[$(i_frozen += 1)])
         else
@@ -198,7 +209,7 @@ end
         @fastmath begin
             @inbounds let ($(flux_unpack...),) = fluxes
                 $(p_assign...)
-                $(expr...)
+                $(ga.statements...)
                 return flux1
             end
         end
