903ac69704ccd6d1b2cbee46d237053919610e01
Fergus Baker <fergusbkr@gmail.com>
2022-08-10 17:13:06 +0100

    pass frozen parameters in fits also

--- src/fitting.jl
+++ src/fitting.jl
@@ -21,21 +21,21 @@ end
 
 function __fitparams!(
     params,
-    model,
+    model::M,
     rm::ResponseMatrix,
     target,
     error_target,
     energy,
     channels;
     kwargs...,
-)
+) where {M<:AbstractSpectralModel}
 
     p0 = get_value.(params)
     lb = get_lowerlimit.(params)
     ub = get_upperlimit.(params)
 
     fit =
-        __lsq_fit(model, p0, lb, ub, rm, target, error_target, energy, channels; kwargs...)
+        __lsq_fit(implementation(M), model, p0, lb, ub, rm, target, error_target, energy, channels; kwargs...)
 
     means = LsqFit.coef(fit)
     errs = LsqFit.stderror(fit)
@@ -48,10 +48,11 @@ function __fitparams!(
     fit
 end
 
-function __lsq_fit(model, p0, lb, ub, rm, target, error_target, energy, channels; kwargs...)
+function __lsq_fit(::AbstractSpectralModelImplementation, model, p0, lb, ub, rm, target, error_target, energy, channels; kwargs...)
+    frozen_p = get_value.(get_frozen_model_params(model))
     fluxes = make_fluxes(energy, flux_count(model))
     foldedmodel(x, p) =
-        @views fold_response(generated_model_call!(fluxes, x, model, p), x, rm)[channels]
+        @views fold_response(invokemodel!(fluxes, x, model, p, frozen_p), x, rm)[channels]
 
     # seems to cause nans and infs
     cov_err = 1 ./ (error_target .^ 2)
@@ -68,3 +69,28 @@ function __lsq_fit(model, p0, lb, ub, rm, target, error_target, energy, channels
     )
     fit
 end
+
+# function __lsq_fit(::JuliaImplementation, model, p0, lb, ub, rm, target, error_target, energy, channels; kwargs...)
+#     frozen_p = get_value.(get_frozen_model_params(model))
+#     d_fluxes = make_dual_fluxes(energy, flux_count(model))
+#     foldedmodel(x, p) = begin
+#         fluxes = map(i -> get_tmp(i, p[1]), d_fluxes)
+#         @views fold_response(invokemodel!(fluxes, x, model, p, frozen_p), x, rm)[channels]
+#     end
+
+#     # seems to cause nans and infs
+#     cov_err = 1 ./ (error_target .^ 2)
+
+#     fit = LsqFit.curve_fit(
+#         foldedmodel,
+#         energy,
+#         target,
+#         cov_err,
+#         p0;
+#         lower = lb,
+#         upper = ub,
+#         autodiff = :forwarddiff,
+#         kwargs...,
+#     )
+#     fit
+# end
