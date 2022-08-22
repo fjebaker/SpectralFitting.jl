f0920b2052c98144d331f4ab8f3d9e7382a42ebf
fjebaker <fergusbkr@gmail.com>
2022-08-22 16:56:51 +0100

    unit bugfixes

--- src/units.jl
+++ src/units.jl
@@ -1,9 +1,32 @@
 module SpectralUnits
+
 using Reexport
 @reexport using Unitful
+
+Unitful.register(SpectralUnits)
 
 @dimension 𝐄 "events" Events
 @refunit counts "counts" Counts 𝐄 false
 
-export counts
+const localpromotion = Unitful.promotion
+function __init__()
+    Unitful.register(SpectralUnits)
+    merge!(Unitful.promotion, localpromotion)
 end
+
+function infer_units(s::Symbol)
+    if s == :rate
+        u"counts / s"
+    elseif s == :counts
+        u"counts"
+    else
+        error("Unknown units $s.")
+    end
+end
+
+export counts
+
+const _counts = typeof(u"counts")
+const _rate = typeof(u"counts / s")
+
+end # module
