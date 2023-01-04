1ad256b182a42619e354ffe778cb41090ac1776b
fjebaker <fergusbkr@gmail.com>
2023-01-04 12:07:37 +0000

    remote introspection tests

--- test/models/test-mutations.jl
+++ test/models/test-mutations.jl
@@ -33,7 +33,16 @@ model = DummyAdditiveTableModel()
 res = SpectralFitting.free_parameters_to_named_tuple(free_params, model)
 @test res == (; K = free_params[1], a = free_params[2])
 
+out = SpectralFitting.all_parameters_to_named_tuple(SpectralFitting.remake_with_number_type(model))
+@test out == (; K=1.0, a=1.0, b=2.0)
+
 outmodel = SpectralFitting.remake_with_number_type(model)
 @test outmodel.K == 1.0
 @test outmodel.a == 1.0
 @test outmodel.b == 2.0
+
+
+# composed models
+model = DummyMultiplicative(a=0.0, b=1.0) * (DummyAdditive(K=2.0, a=3.0) + DummyAdditive(K=4.0, a=5.0))
+out = SpectralFitting.all_parameters_to_named_tuple(model)
+@test out == (;K_1 = 4.0, a_1 = 5.0, b_1 = 5.0, K_2 = 2.0, a_2 = 3.0, b_2 = 5.0, a_3 = 0.0, b_3 = 1.0) 
