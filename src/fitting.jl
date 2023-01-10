ebeea8b736a34ff68c3c0128886635a9a902f267
fjebaker <fergusbkr@gmail.com>
2023-01-10 21:25:28 +0000

    wip fitting wrappers

--- src/fitting.jl
+++ src/fitting.jl
@@ -37,3 +37,34 @@ function wrap_model_noflux(
     end
     energy, wrapped
 end
+
+function wrap_Optimization(
+    model::AbstractSpectralModel,
+    data::SpectralDataset{T};
+    energy = energy_vector(data),
+    target = data.rate,
+    σ = data.rateerror,
+) where {T}
+    ΔE = data.energy_bin_widths
+    # pre-mask the response matrix to ensure channel out corresponds to the active data points
+    R = fold_ancillary(data)[data.mask, :]
+    n = length(target)
+    # pre-allocate the output 
+    wrapped = (params, energy) -> begin
+        flux = invokemodel(energy, model, params)
+        flux = (R * flux)
+        @. flux = flux / ΔE
+        χ2_from_ŷyσ(flux, target, σ)
+        # l = @. flux - target + target * ( log(target) - log(flux) )
+        # -l
+        # l, flux
+    end
+    energy, wrapped
+end
+
+χ2_from_ŷyσ(ŷ, y, σ) = sum(@.((y - ŷ)^2 / σ^2))
+
+function χ2(model::Function, params, data::SpectralDataset; energy = energy_vector(data))
+    ŷ = model(energy, params)
+    χ2_from_ŷyσ(ŷ, data.rate, data.rateerror)
+end
