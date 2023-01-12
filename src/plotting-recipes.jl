8d2254ba8bc952a25b80fa51b013004c58da7a66
fjebaker <fergusbkr@gmail.com>
2023-01-12 09:30:48 +0000

    put observation id as data label

--- src/plotting-recipes.jl
+++ src/plotting-recipes.jl
@@ -2,16 +2,19 @@ using RecipesBase
 
 @recipe function _plotting_func(d::SpectralDataset)
     seriestype := :scatter
-    markersize := 0
-    color := :black
+    markersize := 0.5
+    markershape := :none
     (rate, rateerror) = (d.rate, d.rateerror)
     yerr := rateerror
     xerr := d.energy_bin_widths ./ 2
+    markerstrokecolor := :auto
     xscale := :log10
+    yscale := :log10
     yticks := ([0.01, 0.1, 1, 10, 100], [0.01, 0.1, 1, 10, 100])
     xticks := ([1e-1, 1, 2, 5, 10, 20, 50, 100], [1e-1, 1, 2, 5, 10, 20, 50, 100])
     xlabel := "Energy (keV)"
     ylabel := "counts s⁻¹ keV⁻¹"
+    label := observation_id(d)
     minorgrid := true
     energy = (d.bins_low .+ d.bins_high) ./ 2
     (energy, rate)
@@ -19,7 +22,7 @@ end
 
 # ratio plots
 @userplot RatioPlot
-@recipe function _plotting_func(r::RatioPlot; datacolor=:auto, label="data", modelcolor=:auto)
+@recipe function _plotting_func(r::RatioPlot; datacolor=:auto, modelcolor=:auto)
     if length(r.args) != 2 || !(typeof(r.args[1]) <: SpectralDataset) || !(typeof(r.args[2]) <: AbstractVector)
         error("Ratio plots first argument must be `SpectralDataset` and second argument of type `AbstractVector`.")
     end
@@ -41,7 +44,7 @@ end
     ratio_flux = data.rate ./ model_flux
     @series begin
         markerstrokecolor := datacolor
-        label := label
+        label := observation_id(data)
         seriestype := :scatter
         markershape := :none
         markersize := 0.5
