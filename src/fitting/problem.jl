e9b58b5189c5042e956161be322524a2b75f8c1b
fjebaker <fergusbkr@gmail.com>
2023-01-13 10:03:35 +0000

    basic simultaneous fit (type unstable and no parameter binding)

--- src/fitting/problem.jl
+++ src/fitting/problem.jl
@@ -3,10 +3,70 @@ struct MultiDataset{D}
     MultiDataset(data::Vararg{<:AbstractSpectralDataset}) = new{typeof(data)}(data)
 end
 
+energy_vector(d::MultiDataset) = reduce(vcat, (energy_vector(i) for i in d.d))
+flux_vector(d::MultiDataset) = reduce(vcat, (i.rate for i in d.d))
+std_dev_vector(d::MultiDataset) = reduce(vcat, (i.rateerror for i in d.d))
+
 struct MultiModel{M}
     m::M
     MultiModel(model::Vararg{<:AbstractSpectralModel}) = new{typeof(model)}(model)
 end
+
+function _accumulated_indices(items)
+    total::Int = 0
+    map(items) do item
+        total = item + total
+        total
+    end
+end
+
+function assemble_multimodel(m::MultiModel, d::MultiDataset)
+    n_models = length(m.m)
+    # map on tuple to keep output as tuple and therefore type stable
+    funcs = map((1:n_models...,)) do i
+        model = m.m[i]
+        data = d.d[i]
+        _lazy_folded_invokemodel(model, data)
+    end
+    parameters = map(freeparameters, m.m)
+    autodiff =
+        all(model -> implementation(model) isa JuliaImplementation, m.m) ? :forward :
+        :finite
+
+    # function which accepts all energy and parameters, and then dispatches them correctly to each sub model
+    n_params = _accumulated_indices(map(length, parameters))
+    n_energy = _accumulated_indices(map(data -> length(energy_vector(data)), d.d))
+    n_output = _accumulated_indices(map(data -> length(data.bins_low), d.d))
+    F = (X, params) -> begin
+        fluxes = map(1:n_models) do i
+            start_p = i == 1 ? 1 : n_params[i-1] + 1
+            end_p = n_params[i]
+            p = @views params[start_p:end_p]
+
+            start_x = i == 1 ? 1 : n_energy[i-1] + 1
+            end_x = n_energy[i]
+            x = @views X[start_x:end_x]
+            # invoke wrapped model
+            funcs[i](x, p)
+        end
+        # combine all fluxes into single array
+        reduce(vcat, fluxes)
+    end
+
+    (
+        F,
+        reduce(vcat, parameters),
+        (;
+            funcs = funcs,
+            n_models = n_models,
+            i_out = n_output,
+            i_params = n_params,
+            i_x = n_energy,
+            autodiff = autodiff,
+        ),
+    )
+end
+
 
 struct FittingProblem{M,D}
     model::M
