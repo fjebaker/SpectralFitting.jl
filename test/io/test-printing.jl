36c809366621d028ca205025e5f526a6efee43c5
fjebaker <fergusbkr@gmail.com>
2023-01-14 19:12:10 +0000

    updated test cases

--- test/io/test-printing.jl
+++ test/io/test-printing.jl
@@ -9,11 +9,11 @@ model = DummyAdditive()
 buffer = IOBuffer()
 Base.show(buffer, MIME"text/plain"(), model)
 string = String(take!(buffer))
-expected = """DummyAdditive
-   K  => (1 ± 0.1)
-   a  => (1 ± 0.1)
-   b  => (5 ± 0.5)
-"""
+expected = """┌ DummyAdditive
+│    K  => (1 ± 0.1)
+│    a  => (1 ± 0.1)
+│    b  => (5 ± 0.5)
+└ """
 @test string == expected
 
 # printing composite
@@ -32,21 +32,21 @@ out = @inferred SpectralFitting._destructure_for_printing(model)
 buffer = IOBuffer()
 Base.show(buffer, MIME"text/plain"(), model)
 string = String(take!(buffer))
-expected = """CompositeModel with 3 component models:
-\e[36m     m1 * (a2 + a1)\e[0m
-Model key and parameters:
-   a1 => DummyAdditive
-     K_1 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     a_1 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     b_1 ->  5\e[36m                     FROZEN\e[0m
-   a2 => DummyAdditive
-     K_2 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     a_2 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     b_2 ->  5\e[36m                     FROZEN\e[0m
-   m1 => DummyMultiplicative
-     a_3 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     b_3 ->  5\e[36m                     FROZEN\e[0m
-"""
+expected = """┌ CompositeModel with 3 component models:
+│ \e[36m     m1 * (a2 + a1)\e[0m
+│ Model key and parameters:
+│ \e[36m   a1\e[0m => \e[36mDummyAdditive\e[0m
+│      K_1 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      a_1 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      b_1 ->  5\e[36m                     FROZEN\e[0m
+│ \e[36m   a2\e[0m => \e[36mDummyAdditive\e[0m
+│      K_2 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      a_2 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      b_2 ->  5\e[36m                     FROZEN\e[0m
+│ \e[36m   m1\e[0m => \e[36mDummyMultiplicative\e[0m
+│      a_3 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      b_3 ->  5\e[36m                     FROZEN\e[0m
+└ """
 @test string == expected
 
 
@@ -57,25 +57,25 @@ model =
 buffer = IOBuffer()
 Base.show(buffer, MIME"text/plain"(), model)
 string = String(take!(buffer))
-expected = """CompositeModel with 5 component models:
-\e[36m     (m3 * m2) * a2 + m1 * a1\e[0m
-Model key and parameters:
-   a1 => DummyAdditive
-     K_1 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     a_1 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     b_1 ->  5\e[36m                     FROZEN\e[0m
-   m1 => DummyMultiplicative
-     a_2 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     b_2 ->  5\e[36m                     FROZEN\e[0m
-   a2 => DummyAdditive
-     K_2 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     a_3 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     b_3 ->  5\e[36m                     FROZEN\e[0m
-   m2 => DummyMultiplicative
-     a_4 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     b_4 ->  5\e[36m                     FROZEN\e[0m
-   m3 => DummyMultiplicative
-     a_5 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
-     b_5 ->  5\e[36m                     FROZEN\e[0m
-"""
+expected = """┌ CompositeModel with 5 component models:
+│ \e[36m     (m3 * m2) * a2 + m1 * a1\e[0m
+│ Model key and parameters:
+│ \e[36m   a1\e[0m => \e[36mDummyAdditive\e[0m
+│      K_1 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      a_1 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      b_1 ->  5\e[36m                     FROZEN\e[0m
+│ \e[36m   m1\e[0m => \e[36mDummyMultiplicative\e[0m
+│      a_2 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      b_2 ->  5\e[36m                     FROZEN\e[0m
+│ \e[36m   a2\e[0m => \e[36mDummyAdditive\e[0m
+│      K_2 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      a_3 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      b_3 ->  5\e[36m                     FROZEN\e[0m
+│ \e[36m   m2\e[0m => \e[36mDummyMultiplicative\e[0m
+│      a_4 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      b_4 ->  5\e[36m                     FROZEN\e[0m
+│ \e[36m   m3\e[0m => \e[36mDummyMultiplicative\e[0m
+│      a_5 ->  1 ± 0.1  ∈ [ 0, Inf ]\e[32m   FREE\e[0m
+│      b_5 ->  5\e[36m                     FROZEN\e[0m
+└ """
 @test string == expected
