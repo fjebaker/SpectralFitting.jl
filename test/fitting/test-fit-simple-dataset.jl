f37891b22a808f9eb8abfcc61ba6a86abd66f93b
fjebaker <fergusbkr@gmail.com>
2023-03-24 00:03:57 +0000

    test: simple fitting test

---
+++ test/fitting/test-fit-simple-dataset.jl
@@ -0,0 +1,28 @@
+using Test
+using SpectralFitting
+
+struct DummySimplePowerLaw{T,F} <: AbstractSpectralModel{T,Additive}
+    K::T
+    m::T
+end
+function DummySimplePowerLaw(; K = FitParam(1.0), m = FitParam(1.0))
+    DummySimplePowerLaw{typeof(K),SpectralFitting.FreeParameters{(:K, :m)}}(K, m)
+end
+function SpectralFitting.invoke!(out, x, model::DummySimplePowerLaw)
+    @. out = x + model.m
+end
+SpectralFitting.Δoutput_length(::Type{<:DummySimplePowerLaw}) = 0
+
+# generate a powerlaw simple data
+x = collect(range(1.0, 10.0, 18))
+y = @. 13.12 * x + 42
+data = SimpleDataset("example", x, y)
+
+model = DummySimplePowerLaw()
+
+prob = FittingProblem(model, data)
+
+res = fit(prob, LevenbergMarquadt())
+@test res.u[1] ≈ 13.12 atol = 0.1
+@test res.u[2] ≈ 3.201 atol = 0.1
+
