b9396f97c714f12bdb36eb42ffc1067388a0b085
fjebaker <fergusbkr@gmail.com>
2023-10-01 03:57:20 +0100

    test: allocation test to 0

--- test/fitting/test-cache.jl
+++ test/fitting/test-cache.jl
@@ -4,7 +4,7 @@ using SpectralFitting, Test
 dummy_data = make_dummy_dataset((E) -> (E^(-3.0)); units = u"counts / (s * keV)")
 
 # test that both julia and xspec implementations can fit simple
-model = PhotoelectricAbsorption() * PowerLaw()
+model = PhotoelectricAbsorption() * PowerLaw() + PowerLaw() + PowerLaw()
 
 config = SpectralFitting.FittingConfig(model, dummy_data)
 
@@ -16,4 +16,4 @@ result = f(domain, params)
 # todo: currently we still allocate cus extracting the frozen parameters
 # has a dynamic allocation
 allocated_bytes = @allocated f(domain, params)
-@test allocated_bytes < 400
+@test allocated_bytes == 0
