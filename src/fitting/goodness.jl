4ffd4831f976906792b05b82ed1736b6d6646956
fjebaker <fergusbkr@gmail.com>
2024-05-12 16:26:12 +0100

    feat(fit): goodness now can refit the model

--- src/fitting/goodness.jl
+++ src/fitting/goodness.jl
@@ -1,25 +1,29 @@
 
 function goodness(
-    result::FittingResultSlice,
+    result::AbstractFittingResult,
     u::AbstractVector{T},
     σ::AbstractVector{T};
     N = 1000,
     stat = ChiSquared(),
     distribution = Distributions.Normal,
+    refit = true,
 ) where {T}
-    x = similar(u)
     measures = zeros(T, N)
-
+    config = deepcopy(result.config)
     for i in eachindex(measures)
         # sample the next parameters
         for i in eachindex(u)
             m = u[i]
             d = σ[i]
             # TODO: respect the upper and lower bounds of the parameters
-            # Distributions.Truncated(distribution(mean, dev), ) 
-            x[i] = rand(distribution(m, d))
+            distr = Distributions.Truncated(distribution(m, d), get_lowerlimit(config.parameters[i]), get_upperlimit(config.parameters[i])) 
+            set_value!(config.parameters[i], rand(distr))
         end
-        measures[i] = measure(stat, result, x)
+
+        if refit
+            new_result = fit(config, LevenbergMarquadt())
+        end
+        measures[i] = measure(stat, new_result)
     end
 
     perc = 100 * count(<(result.χ2), measures) / N
@@ -28,9 +32,9 @@ function goodness(
     measures
 end 
 
-function goodness(result::FittingResult, σu = result.σu; kwargs...)
-    @assert !isnothing(result.σu) "σ cannot be nothing, else algorithm has no parameter intervals to sample from."
-    goodness(result[1], result.u, σu; kwargs...)
+function goodness(result::AbstractFittingResult, σu = estimated_error(result); kwargs...)
+    @assert !isnothing(σu) "σ cannot be nothing, else algorithm has no parameter intervals to sample from."
+    goodness(result, estimated_params(result), σu; kwargs...)
 end
 
 export goodness
